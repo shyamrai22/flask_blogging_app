77f11b9c9a27e8bd909c142e6be7843c96bbfc05
Shyam Rai <shyamrrai0011@gmail.com>
2025-07-02 12:43:51 +0530

    Initial commit of my flask based blogging app

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,8 @@
 class Config:
-    SECRET_KEY = "62cefb3c557c6481a50f9a336b6f66ad"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SECRET_KEY = "your_secret_key"
+    SQLALCHEMY_DATABASE_URI = "your_db_link"
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "shyamrr0011@gmail.com"
-    MAIL_PASSWORD = "pogn otsv uyut srjt"
+    MAIL_USERNAME = "your_email"
+    MAIL_PASSWORD = "your_bit_password"
